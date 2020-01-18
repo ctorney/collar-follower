774266d289d9fef4f794e5046fa9df45bc7a4942
ctorney <colin.j.torney@gmail.com>
2020-01-18 12:20:27 +0000

    bash script working

--- tag_scripts/send_to_tag.sh
+++ tag_scripts/send_to_tag.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
-# Simple line count example, using bash
+# Script for sending code to mataki tags
 #
-# Bash tutorial: http://linuxconfig.org/Bash_scripting_Tutorial#8-2-read-file-into-bash-array
-# My scripting link: http://www.macs.hw.ac.uk/~hwloidl/docs/index.html#scripting
-#
-# Usage: ./line_count.sh file
+# Steps for using:
+# 1. Connect mataki tag via support board and usb port
+# 2. Connect to serial comm with screen: screen /dev/ttyUSB0 115200
+# 3. Switch on tag with support board switch SW2
+# 4. Hit C on screen console to prevent script start-up
+# 5. Disconnect from screen ctrl+a+k
+# 6. Run this script, usage: ./send_to_tag.sh file
 # -----------------------------------------------------------------------------
 
-# Link filedescriptor 10 with stdin
-# remember the name of the input file
 filename=$1
 
-extension="${filename##*.}"
 savename="${filename%.*}"
-echo $savename
-echo $filename
 
-echo "NEW"
-# echo "NEW" >> /dev/ttyUSB0
-cat $filename
-# cat $filename >> /dev/ttyUSB0
-echo "SAVE $savename"
-# echo "SAVE $savename" >> /dev/ttyUSB0
+stty -F /dev/ttyUSB0 115200 raw -echo -echoe -echok -echoctl -echoke
 
+# send NEW command to clear the current script
+echo -n -e "NEW\r"  >> /dev/ttyUSB0
 
+# loop over the file and send new script line by line
+while read -r line
+do
+  echo -n -e "$line\r" >> /dev/ttyUSB0 
+  #echo -e "$line\r" 
+  sleep 0.1
+done < "$filename"
+
+# send the SAVE command to write to flash memory
+echo -n -e "SAVE \"$savename\"\r"  >> /dev/ttyUSB0
