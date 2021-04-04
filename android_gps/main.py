98f8aa921dda6d10415319b81fcbaa611b667fc9
Colin Torney <colin.j.torney@gmail.com>
2021-04-04 14:39:00 +0100

    message viewer and fixed layout of connect and spoof buttons

--- android_gps/main.py
+++ android_gps/main.py
@@ -5,29 +5,58 @@ from kivy.clock import Clock
 
 from kivy.uix.switch import Switch
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 from kivy.lang import Builder
 from jnius import autoclass
 from oscpy.client import OSCClient
 import android
 
 
-# A Gridlayout with a label a switch
-class gui(GridLayout):
+from kivy.graphics import Color, Rectangle
 
-     def __init__(self, switch_callback, **kwargs):
+from kivy.core.window import Window
 
-          super(gui, self).__init__(**kwargs)
+from datetime import datetime
 
-          self.cols = 2
+class gui(BoxLayout):
 
-          self.add_widget(Label(text="Spoof GPS:"))
+    def __init__(self, gps_switch_callback, ble_switch_callback, **kwargs):
 
-          self.settings_sample = Switch(active=False)
+        super(gui, self).__init__(orientation='vertical',**kwargs)
 
-          self.add_widget(self.settings_sample)
 
-          self.settings_sample.bind(active=switch_callback)
+        w,h = Window.size
+
+        horizontalBox   = BoxLayout(orientation='horizontal',size_hint_y=0.25)
+
+        self.ble_info = Label(text="Connect to bluetooth:")
+        horizontalBox.add_widget(self.ble_info)
+
+        self.ble_switch = Switch(active=False)
+
+        horizontalBox.add_widget(self.ble_switch)
+
+        self.ble_switch.bind(active=ble_switch_callback)
+
+        horizontalBox.add_widget(Label(text="Start GPS spoofing:"))
+
+        self.gps_switch = Switch(active=False)
+
+        horizontalBox.add_widget(self.gps_switch)
+
+        self.gps_switch.bind(active=gps_switch_callback)
+        self.add_widget(horizontalBox)
+
+        self.scrollview = ScrollView(do_scroll_x=True, scroll_type=["bars","content"],size_hint_y=0.75)#, height=0.5*h)
+        self.messages = Label(text="..",font_size="20sp", text_size=(int(0.9*w), None), size_hint=(1,None))#, height=0.5*h)
+
+        self.messages.bind(texture_size=lambda *x: self.messages.setter('height')(self.messages, self.messages.texture_size[1]))
+
+        self.scrollview.add_widget(self.messages)
+        self.add_widget(self.scrollview)
 
 
 
@@ -66,13 +95,28 @@ class collarFollower(App):
         self.service = service
 
         self.client = OSCClient(b'localhost', 3000)
-        return gui(self.switch_callback)
+ 
+        return gui(self.gps_switch_callback, self.ble_switch_callback)
 
-    def switch_callback(self, switchObject, switchValue):
+    def gps_switch_callback(self, switchObject, switchValue):
 
-        print('Value of sample settings is:', switchValue)
-        self.client.send_message(b'/button', [switchValue])
+        print('Value of gps settings is:', switchValue)
+        #self.client.send_message(b'/button', [switchValue])  # uncomment to enable spoofing
 
+        self.root.messages.text = "[" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "] Here's another message from the device.\n" + self.root.messages.text
+
+    def ble_switch_callback(self, switchObject, switchValue):
+
+        if switchValue:
+            self.root.ble_info.text = 'Connecting....'
+        else:
+            self.root.ble_info.text = 'Connect to bluetooth:'
+
+        self.root.messages.text = "[" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "] Here's another message from the device.\n" + self.root.messages.text
+        print('Value of ble sample settings is:', switchValue)
+
+    def on_start(self):
+        return
 
     def on_pause(self):
         return True
