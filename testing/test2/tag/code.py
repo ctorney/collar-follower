66b6d9ba1ba84521a3f8fa8cb13907b9fd7ac88a
cyruskavwele12 <58072022+cyruskavwele12@users.noreply.github.com>
2021-10-28 15:40:50 +0100

    tag sending coordinates to BS

--- testing/test2/tag/code.py
+++ testing/test2/tag/code.py
@@ -1,7 +1,9 @@
 # receive message to start and then start sending GPS, until receive stop message
 
-
-# Simple GPS module demonstration Will wait for a fix and print a message every second with the current location and other details.
+# Author: Cyrus, Colin, Grant
+# Simple GPS module demonstration.
+# Will wait for a fix and print a message every second with the current location
+# and other details.
 import time
 import board
 import busio
@@ -9,8 +11,15 @@ import busio
 from time import sleep
 
 import adafruit_gps
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# Simple demo of sending and recieving data with the RFM95 LoRa radio.
+# Author: Tony DiCola
 import digitalio
+
 import adafruit_rfm9x
+
 # Define radio parameters.
 RADIO_FREQ_MHZ = 869.45  # Frequency of the radio in Mhz. Must match your
 # module! Can be a value like 915.0, 869.45, etc.
@@ -34,52 +43,41 @@ uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
 gps = adafruit_gps.GPS(uart, debug=False)  # Use UART/pyserial
 
 
-collar_ID = "the collar_ID is WB100"
-
 #name the collar ID
+collar_ID = "Collar_ID WB100"
 while True:
-    packet = rfm9x.send(bytes(collar_ID + '\n', "utf-8"))  # send via radio
-    print("collar_id send")
-    time.sleep(2)
+    packet = rfm9x.send(bytes(collar_ID, "utf-8"))  # send via radio
+    print("Hi, collar ID send")
+    # sleep(1.0)
 
-    # receive wake up message
-    packet =rfm9x.receive()
+    #~ receive gps start message
+    packet = rfm9x.receive(timeout=1.0)
     if packet is not None:
-        print(packet) # print received packet
+        packet_text = str(packet, "ascii")
+        print("Received (ASCII):\n {0}".format(packet_text))
+        # print(packet)
+        if packet_text == "AAA":
+            print("GPS intiated")
+            message = "broadcast mode intiated"
+            packet = rfm9x.send(bytes(message, "utf-8"))  # send via radio
+            # print(packet)
+            # Turn on just minimum info (RMC only, location):
+            gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+            # Set update rate to once a second (1hz) which is what you typically want.
+            gps.send_command(b"PMTK220,1000")
 
-    if packet is not None:
-        try:
-            txtpacket = packet.decode()
-            textpacket = txtpacket.split(",")
-            if txtpacket [0] == "BASE,PING":
-                message = "broadcast mode intiated"
-                packet = rfm9x.send(bytes(message + '\n', "utf-8"))  # send via radio
-                print(packet)
-                # Turn on just minimum info (RMC only, location):
-                gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-                # Set update rate to once a second (1hz) which is what you typically want.
-                gps.send_command(b"PMTK220,1000")
-
-                # ~~~~~Main loop runs forever printing the location, etc. every second.
-                last_print = time.monotonic()
-                while True:
-                    sentence = gps._read_sentence()
-                    if sentence is None:
-                        continue
-                    print(sentence)
-                    #rfm9x.send(bytes(sentence + '\n', "utf-8"))
-                    # sleep(0.1)
-            elif packet == GPSSTOP:
-                print("GPS has stopped")
-                gps.send_command(b"PMTK161,0") # GPS enters into low power mode
-                packet = rfm9x.send(bytes("GPS low power mode initiated", "utf-8"))
-                print("GPS low power mode initiated")
-        except:
-            pass
-
-
-
-
-
+            # ~~~~~Main loop runs forever printing the location, etc. every second.
+            last_print = time.monotonic()
+            while True:
+                gps.update()
+                sentence = gps._read_sentence()
+                if sentence is None:
+                    continue
+                ns = sentence.split(",")
+                if ns[2] == "A":
+                    ns=(ns[3]+ns[4] + "\n"+ ns[5]+ns[6])
+                    print(ns)
+                    rfm9x.send(bytes(ns + '\n', "utf-8"))
+        #sleep(0.1)
 
 
