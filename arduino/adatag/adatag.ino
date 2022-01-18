d78af81303ddd991da3bb115bef18d31301fce5f
Colin Torney <colin.j.torney@gmail.com>
2022-01-18 18:02:01 +0000

    working radio and gps only

--- arduino/adatag/adatag.ino
+++ arduino/adatag/adatag.ino
@@ -1,11 +1,141 @@
 
 
-// rf95_reliable_datagram_client.pde
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple addressed, reliable messaging client
-// with the RHReliableDatagram class, using the RH_RF95 driver to control a RF95 radio.
-// It is designed to work with the other example rf95_reliable_datagram_server
-// Tested with Anarduino MiniWirelessLoRa, Rocket Scream Mini Ultra Pro with the RFM95W 
+#include <SAMDTimerInterrupt.h>
+#include <SAMD_ISR_Timer.h>
+
+ 
+#include <RHReliableDatagram.h>
+#include <RH_RF95.h>
+#include <SPI.h>
+
+#define GPS_INTERRUPT_MS        10     // 1s = 1000ms
+#define NMEA_SENTENCE_MAX_LEN   82     
+
+
+
+ // for feather m0  
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+
+#define RF95_FREQ 868.0
+
+
+#define CLIENT_ADDRESS 1
+#define SERVER_ADDRESS 2
+ 
+
+
+
+#include <Adafruit_GPS.h>
+
+// what's the name of the hardware serial port?
+#define GPSSerial Serial1
+
+// Connect to the GPS on the hardware port
+Adafruit_GPS GPS(&GPSSerial);
+
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO false
+
+uint32_t timer = millis();
+
+
+// Radio driver and manager
+RH_RF95 driver(RFM95_CS, RFM95_INT);
+RHReliableDatagram manager(driver, CLIENT_ADDRESS);
+
+
+
+void gps_interrupt(void)
+{
+    GPS.read();
+}
+
+void setup()
+{
+  return;
+  Serial.begin(115200);
+  Serial.println("Adafruit GPS library basic parsing test!");
+  // Init timer ITimer1
+  SAMDTimer ITimer0(TIMER_TC3);  
+  // Interval in microsecs
+  if (ITimer0.attachInterruptInterval(GPS_INTERRUPT_MS * 1000, gps_interrupt))
+    Serial.println("Starting  ITimer0 OK, millis() = " + String(millis()));
+  else
+    Serial.println("Can't set ITimer0. Select another freq. or timer");
+  
+
+  // GPS initialise
+  GPS.begin(9600);
+
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  //GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_100_MILLIHERTZ); // 1 Hz update rate
+  
+  delay(1000);
+
+  if (!manager.init())
+    Serial.println("init failed");
+  
+  if (!driver.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // LoRa settings
+  driver.setModemConfig(RH_RF95::Bw125Cr45Sf128);
+  driver.setTxPower(23, false);
+  manager.setRetries(0);
+  delay(1000);
+  
+}
+
+uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+
+void loop() // run over and over again
+{
+    return;
+
+  if (GPS.newNMEAreceived()) {
+    Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+    
+    if (manager.sendtoWait((uint8_t *)GPS.lastNMEA(), NMEA_SENTENCE_MAX_LEN, SERVER_ADDRESS))
+    {
+          Serial.println("sendtoWait succeeded");
+      
+// Now wait for a reply from the server
+    uint8_t len = sizeof(buf);
+    uint8_t from;   
+    if (manager.recvfromAckTimeout(buf, &len, 900, &from))
+    {
+      Serial.print("got reply from : 0x");
+      Serial.print(from, HEX);
+      Serial.print(": ");
+      Serial.println((char*)buf);
+  
+  
+    }
+          
+    }
+    else
+      Serial.println("sendtoWait failed");
+  }
+    
+
+  
+}
+
+/*
+
+#include <Adafruit_PMTK.h>
+#include <Adafruit_GPS.h>
+#include <NMEA_data.h>
+
  
 #include <RHReliableDatagram.h>
 #include <RH_RF95.h>
@@ -24,60 +154,29 @@
 #define CLIENT_ADDRESS 1
 #define SERVER_ADDRESS 2
  
-// Singleton instance of the radio driver
-//RH_RF95 driver;
 
-// Singleton instance of the radio driver
+// Radio driver and manager
 RH_RF95 driver(RFM95_CS, RFM95_INT);
-//RH_RF95 driver(5, 2); // Rocket Scream Mini Ultra Pro with the RFM95W
- 
-// Class to manage message delivery and receipt, using the driver declared above
 RHReliableDatagram manager(driver, CLIENT_ADDRESS);
  
-// Need this on Arduino Zero with SerialUSB port (eg RocketScream Mini Ultra Pro)
-//#define Serial SerialUSB
  
 void setup() 
 {
-  // Rocket Scream Mini Ultra Pro with the RFM95W only:
-  // Ensure serial flash is not interfering with radio communication on SPI bus
-//  pinMode(4, OUTPUT);
-//  digitalWrite(4, HIGH);
- 
   Serial.begin(9600);
   while (!Serial) ; // Wait for serial port to be available
   if (!manager.init())
     Serial.println("init failed");
-   if (!driver.setFrequency(RF95_FREQ)) {
+  
+  if (!driver.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
     while (1);
   }
+  
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-  // rf95.setModemConfig(RH_RF95::Bw31_25Cr48Sf512);
+  // LoRa settings
   driver.setModemConfig(RH_RF95::Bw125Cr45Sf128);
-
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
-  // you can set transmitter powers from 5 to 23 dBm:
   driver.setTxPower(23, false);
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
- 
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
-  // you can set transmitter powers from 2 to 20 dBm:
-//  driver.setTxPower(20, false);
-  // If you are using Modtronix inAir4 or inAir9, or any other module which uses the
-  // transmitter RFO pins and not the PA_BOOST pins
-  // then you can configure the power transmitter power for 0 to 15 dBm and with useRFO true. 
-  // Failure to do that will result in extremely low transmit powers.
-//  driver.setTxPower(14, true);
- 
-  // You can optionally require this module to wait until Channel Activity
-  // Detection shows no activity on the channel before transmitting by setting
-  // the CAD timeout to non-zero:
-//  driver.setCADTimeout(10000);
   manager.setRetries(0);
 }
  
